22e6eb263e6c69dc339103ca0704aa4e0e73b3c7
Andres Gomez <andregom2009@gmail.com>
2024-05-21 15:53:54 +0200

    final changes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,8 +1,51 @@
-const router = require('express').router();
+const router = require('express').Router();
 const fs = require('fs')
-const path = require('path')
-const  uuid  = require('uuid');
+const {v4: uuidv4} = require('uuid');
 
-router.get('/api/notes', (req, res) => {
-    
+router.get("/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data)=> {
+    res.json(JSON.parse(data))
+  });
+   
 });
+
+router.post("/notes", (req, res) => {
+  fs.readFile("db/db.json",'utf8', (err, data)=> {
+
+    const dbJson = JSON.parse(data);
+    
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4(),
+    };
+
+    dbJson.push(newNote);
+
+    fs.writeFile("db/db.json", JSON.stringify(dbJson), (err, result)=>{
+      res.json('file written');
+  
+    });
+  })
+
+});
+router.delete('/notes/:id', (req, res) => {
+  
+  fs.readFile("db/db.json", 'utf8', (err, data)=>{
+    
+    const dataJSON = JSON.parse(data);
+    
+    const newNotes = dataJSON.filter((note) =>  note.id !== req.params.id);
+
+    fs.writeFile("db/db.json", JSON.stringify(newNotes), (err, result)=> {
+
+      res.json("Note deleted.");
+    });
+
+  })
+  
+
+
+});
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const html_routes = require('./routes/notes-index');
 const api_routes = require('./routes/api_routes');
-const paths = require('path');
 
-const PORT =  5001;
+const PORT = process.env.PORT || 5001;
 const app = express();
 
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+
+app.use('/api',api_routes)
+app.use('/',html_routes)
 
 app.listen(PORT, () => {
     console.log(`Working on http://localhost:${PORT}`);
